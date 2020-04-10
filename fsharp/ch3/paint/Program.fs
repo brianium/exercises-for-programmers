b2b01d753877834edeb97720587e9bca35dd4476
Brian Scaturro <scaturrob@gmail.com>
2020-04-10 11:55:17 -0400

    fsharp: paint cli

---
+++ fsharp/ch3/paint/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Paint.Cli
+
+[<EntryPoint>]
+let main argv =
+    run ()
+    0 // return an integer exit code
---
+++ fsharp/ch3/paint/src/Cli.fs
@@ -0,0 +1,52 @@
+namespace Paint
+
+module Cli =
+    open Input.Cli
+
+    module RoomType =
+
+        type T =
+            | Rectangle
+            | Round
+            | LShaped
+
+        let create (s:string) =
+            let lower = s.ToLower()
+            match lower with
+            | "round" -> Round
+            | "l-shaped" -> LShaped
+            | _ -> Rectangle
+
+        let label t =
+            match t with
+            | Rectangle -> "rectangle"
+            | Round -> "round"
+            | LShaped -> "l-shaped"
+
+    let promptRoom () =
+        "What type of ceiling are you painting? (round|l-shaped|rectangle)"
+        |> prompt
+        |> RoomType.create
+
+    let promptDimensions r =
+        RoomType.label r |> printfn "Using ceiling shape: %s"
+        match r with
+        | RoomType.Rectangle -> [askUnsignedFloat "What is the length of the ceiling?";askUnsignedFloat "What is the width of the ceiling?"]
+        | RoomType.Round -> [askUnsignedFloat "What is the diameter of the ceiling?"]
+        | RoomType.LShaped -> [
+                askUnsignedFloat "What is the length of the L's first side?"
+                askUnsignedFloat "What is the width of the L's first side?"
+                askUnsignedFloat "What is the length of the L's second side?"
+                askUnsignedFloat "What is the width of the L's second side?"
+            ]
+
+    let display room =
+        let area = Room.area room
+        Room.gallonsRequired area |> printfn "You will need to purchase %d gallons of"
+        area.ToString("0.##") |> printfn "paint to cover %s square feet"
+
+    let run () =
+        promptRoom ()
+        |> promptDimensions
+        |> Room.fromList
+        |> display
