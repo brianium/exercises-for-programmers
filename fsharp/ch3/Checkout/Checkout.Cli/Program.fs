552783ba5f47e5ebafdca28ea8e9a0e10bf55f6a
Brian Scaturro <scaturrob@gmail.com>
2020-04-15 07:39:29 -0400

    fsharp: self checkout

---
+++ fsharp/ch3/Checkout/Checkout.Cli/Program.fs
@@ -0,0 +1,78 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.Threading
+open Input.Cli
+open Checkout.Domain
+
+/// View
+
+let display order =
+    printfn ""
+    order.SubTotal |> printfn "Subtotal: $%0.2f" 
+    order.TaxRate * order.SubTotal |> printfn "Tax: $%0.2f"
+    Order.total order |> printfn "Total: $%0.2f"
+
+let controlKey =
+    match int Environment.OSVersion.Platform with
+    | 6 -> "Cmd"
+    | _ -> "Ctrl"
+
+/// Async Order state
+
+type Msg =
+    | Update of Order
+    | Checkout
+
+let update (agent: MailboxProcessor<Msg>) order =
+    Update order |> agent.Post
+    order
+
+let checkout (cts: CancellationTokenSource) (order: Order) = 
+    let o = Order.checkout order
+    display o
+    cts.Cancel()
+    o
+
+let createAgent (cts: CancellationTokenSource) (order: Order) =
+    MailboxProcessor.Start((fun inbox ->
+        let rec messageLoop prevOrder =
+            async {
+                let! msg = inbox.Receive()
+
+                let newState = match msg with
+                               | Update o -> o
+                               | Checkout -> checkout cts prevOrder
+
+                return! messageLoop newState
+            }
+        messageLoop order), cts.Token)
+
+/// Run that bad brother
+
+let rec takeOrder (agent: MailboxProcessor<Msg>) order: Order =
+    let num = order.LineItems.Length + 1
+    let (price,quantity) = (
+        sprintf "Enter the price of item %d:" num |> askUnsignedFloat,
+        sprintf "Enter the quantity of item %d:" num |> askUnsignedInt
+    )
+    LineItem.create price quantity 
+    |> Order.addItem order
+    |> update agent
+    |> takeOrder agent
+
+[<EntryPoint>]
+let main argv =
+    use cts = new CancellationTokenSource()
+    let order = Order.create 0.055
+    let agent = createAgent cts order
+
+    Console.CancelKeyPress.Add(
+        fun e -> agent.Post Checkout
+                 cts.Token.WaitHandle.WaitOne() |> ignore
+    )
+
+    printfn "Press %s + C to complete checkout" controlKey
+    takeOrder agent order |> ignore
+
+    0 // return an integer exit code
---
+++ fsharp/ch3/Checkout/Checkout.Domain/Library.fs
@@ -0,0 +1,31 @@
+﻿namespace Checkout.Domain
+
+type LineItem =
+    { Price: float
+      Quantity: int }
+
+type Order =
+    { LineItems: List<LineItem>
+      SubTotal: float
+      TaxRate: float }
+
+module LineItem =
+    let create price quantity =
+        { Price = price
+          Quantity = quantity }
+
+    let total init { Quantity = q; Price = p } = p * float q + init
+
+module Order =
+    let create taxRate =
+        { LineItems = []
+          SubTotal = 0.0
+          TaxRate = taxRate }
+
+    let addItem order lineItem = { order with LineItems = order.LineItems @ [ lineItem ] }
+
+    let total { SubTotal = s; TaxRate = t } =
+        s * t + s
+
+    let checkout order =
+        { order with SubTotal = List.fold LineItem.total 0.0 order.LineItems }
