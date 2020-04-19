2c0200ee36ac80034c1ed0d5c4faa517771b4af9
Brian Scaturro <scaturrob@gmail.com>
2020-04-19 19:40:54 -0400

    fsharp: simple interest

---
+++ fsharp/ch3/SimpleInterest/SimpleInterest.Cli/Program.fs
@@ -0,0 +1,41 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open SimpleInterest.Domain
+
+let rec askFloat prompt asPercent =
+    printf "%s " prompt
+    let input = stdin.ReadLine() |> (fun x -> x.Trim())
+    try
+        match Double.TryParse(input) with
+        | true, x when asPercent && x >= 0.0 -> x / 100.0
+        | true, x when not asPercent && x >= 0.0 -> x
+        | _ -> failwith "Error parsing float"
+    with
+    | _ ->  printfn "%s is not a positive numeric value" input
+            askFloat prompt asPercent
+
+let rec askInt prompt =
+    printf "%s " prompt
+    let input = stdin.ReadLine() |> (fun x -> x.Trim())
+    try
+        match Int32.TryParse(input) with
+        | true, x -> x
+        | _ -> failwith "Error parsing int"
+    with
+    | _ -> printfn "%s is not a numeric value" input
+           askInt prompt
+
+let displayReturn (roi: Return) =
+    printfn "After %i year(s) at %0.2f%%, the investment will be worth $%0.2f" roi.Years (roi.Investment.InterestRate * 100.0) roi.Value
+
+let display (roi: seq<Return>) =
+    Seq.iter displayReturn roi
+
+[<EntryPoint>]
+let main argv =
+    (askFloat "Enter the principal:" false, askFloat "Enter the rate of interest:" true)
+    |> Investment.create
+    |> Investment.calculateYearlySimpleInterest (askInt "Enter the number of years:")
+    |> display
+    0 // return an integer exit code
---
+++ fsharp/ch3/SimpleInterest/SimpleInterest.Domain/Library.fs
@@ -0,0 +1,39 @@
+﻿namespace SimpleInterest.Domain
+
+type Investment =
+    { Principal: float
+      InterestRate: float }
+
+type Return =
+    { Investment: Investment
+      Years: int
+      Value: float }
+
+module Investment =
+    open System
+
+    let createReturn investment years value =
+        { Investment = investment
+          Years = years
+          Value = value }
+
+    let private divBy100 x = x / 100.0
+
+    let calculateSimpleInterest years investment =
+        1.0 + investment.InterestRate * float years
+        |> (*) investment.Principal
+        |> (*) 100.0
+        |> Math.Ceiling
+        |> divBy100
+        |> createReturn investment years
+
+    let calculateYearlySimpleInterest years investment =
+      seq { for i in 1..years -> i }
+      |> Seq.map (fun y -> calculateSimpleInterest y investment)
+
+    let create (principal, rate) =
+        match rate with
+        | x when x < 0.0 || x > 1.0 -> failwith "rate must be between 0.0 and 1.0 (inclusive)"
+        | _ ->
+            { Principal = principal
+              InterestRate = rate }
