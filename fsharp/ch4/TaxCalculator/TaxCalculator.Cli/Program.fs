378ebff9a422a23739f2d0b5585ca7c3fba7e93f
Brian Scaturro <scaturrob@gmail.com>
2020-04-21 19:39:45 -0400

    fsharp: tax calculator

---
+++ fsharp/ch4/TaxCalculator/TaxCalculator.Cli/Program.fs
@@ -0,0 +1,36 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open TaxCalculator.Domain
+
+let prompt text =
+    printf "%s " text
+    stdin.ReadLine() |> (fun x -> x.Trim())
+
+let rec promptFloat text =
+    let x = prompt text
+    try 
+        match Double.TryParse x with
+        | true, f when f > 0.0 -> f
+        | _ -> failwith "Value must be numeric and greater than 0"
+    with
+        | ex -> printfn "%s" ex.Message
+                promptFloat text
+
+let formatCurrency (x:decimal) = x.ToString("C")
+
+let display (t: Transaction) =
+    match t.TaxRate with
+    | x when x > 0.0 ->  Transaction.toNearestCent t.SubTotal |> formatCurrency |> printfn "The subtotal is %s."
+                         Transaction.tax t |> decimal |> formatCurrency |> printfn "The tax is %s."
+    | _ -> ()
+    Transaction.total t |> formatCurrency |>
+     printfn "The total is %s."
+
+[<EntryPoint>]
+let main argv =
+    (promptFloat "What is the order amount?", prompt "What is the state?")
+    |> Order.create
+    |> Order.complete 5.5
+    |> display
+    0 // return an integer exit code
---
+++ fsharp/ch4/TaxCalculator/TaxCalculator.Domain/Library.fs
@@ -0,0 +1,41 @@
+﻿namespace TaxCalculator.Domain
+
+type Order =
+    { Amount: float
+      State: string }
+
+type Transaction =
+    { Order: Order
+      SubTotal: float
+      TaxRate: float }
+
+module Order =
+    let create (amount, state) =
+        { Amount = amount
+          State = state }
+
+    let complete tax order =
+        match order.State.ToLower() with
+        | "wi" | "wisconsin" when tax >= 0.0 && tax <= 100.00 -> { Order = order; SubTotal = order.Amount; TaxRate = tax }
+        | _ when tax < 0.0 || tax > 100.0 -> failwith "Tax rate must be between 0.0 and 100.0 inclusive"
+        | _ -> { Order = order; SubTotal = order.Amount; TaxRate = 0.0 }
+
+module Transaction =
+    open System
+
+    let private divBy100 x = x / 100.0
+
+    let toNearestCent f =
+        f * 100.00
+        |> Math.Ceiling
+        |> divBy100
+        |> decimal
+
+    let tax { TaxRate = t; SubTotal = s } =
+        t / 100.0
+        |> (*) s
+
+    let total transaction =
+        tax transaction
+        |> (+) transaction.SubTotal
+        |> toNearestCent
