fbc0868080253791fb282ca5e70394377ac4856f
Brian Scaturro <scaturrob@gmail.com>
2020-04-24 15:38:16 -0400

    fsharp: password validator

---
+++ fsharp/ch4/PasswordValidator/PasswordValidator.Cli/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open PasswordValidator.Domain
+
+[<EntryPoint>]
+let main argv =
+    ReadLine.ReadPassword "What is your secret? "
+    |> Users.lookup
+    |> function
+       | Some x -> printfn "Welcome %s!" x
+       | None -> printfn "I don't know you"
+    0 // return an integer exit code
---
+++ fsharp/ch4/PasswordValidator/PasswordValidator.Domain/Library.fs
@@ -0,0 +1,28 @@
+﻿namespace PasswordValidator.Domain
+
+module Password =
+    open System
+    open BCrypt.Net
+
+    let randomSalt() = BCrypt.GenerateSalt 12
+
+    let hash str = BCrypt.HashPassword(str, randomSalt())
+
+    let validate check hash = BCrypt.Verify(check, hash)
+
+module Users =
+    open Password
+
+    let passwords =
+        dict
+            [ hash "secret", "Brian"
+              hash "password", "John"
+              hash "P@ssw0rd!", "Ishmel" ]
+
+    let lookup password = 
+        try
+            Seq.find (fun k -> validate password k) passwords.Keys
+            |> function
+               | x -> passwords.Item x |> Some
+        with
+        | _ -> None
