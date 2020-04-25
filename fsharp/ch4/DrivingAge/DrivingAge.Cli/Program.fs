7bf45eca8c06939070d9e99d7163f9131a81509d
Brian Scaturro <scaturrob@gmail.com>
2020-04-25 09:14:02 -0400

    fsharp: legal driving age

---
+++ fsharp/ch4/DrivingAge/DrivingAge.Cli/Program.fs
@@ -0,0 +1,29 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open DrivingAge.Domain
+open DrivingAge.Domain.Driving
+
+let read text = 
+    printf "%s " text
+    stdin.ReadLine() |> (fun s -> s.Trim())
+
+let rec askInt text =
+    let intString = read text
+    try
+        match Int32.TryParse intString with
+        | true, x when x > 0 -> x
+        | _ -> failwith "Age must be a positive integer greater than 0"
+    with
+    | e -> printfn "%s" e.Message
+           askInt text
+
+[<EntryPoint>]
+let main argv =
+    askInt "What is your age?"
+    |> getLegalCountries
+    |> function
+       | [] -> printfn "You are not old enough to drive in any of the known countries"
+       | (list: List<Country>) -> printfn "You can drive in the following countries:"
+                                  List.iter (fun c -> printfn "%s" c.Name) list
+    0 // return an integer exit code
---
+++ fsharp/ch4/DrivingAge/DrivingAge.Domain/Library.fs
@@ -0,0 +1,22 @@
+﻿namespace DrivingAge.Domain
+
+type Country =
+    { Name: string
+      LegalDrivingAge: int }
+
+module Country =
+    let create name age =
+        { Name = name
+          LegalDrivingAge = age }
+
+    let knownCountries =
+        [ create "USA" 16
+          create "Argentina" 17
+          create "Honduars" 18
+          create "France" 15 ]
+
+module Driving =
+    open Country;
+
+    let getLegalCountries age =
+        List.filter (fun { LegalDrivingAge = l } -> l <= age) knownCountries
